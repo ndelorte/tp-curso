284dec5b755858263bf77b8f1c8769f1a6f082d6
Nicolas Delorte <delorte85@gmail.com>
2023-05-04 09:46:34 -0300

    modificar y eliminar

--- backend/routes/admin/novedades.js
+++ backend/routes/admin/novedades.js
@@ -42,5 +42,45 @@ router.post('/agregar', async (req, res, next) => {
 })
 
 
+/*eliminar */
+
+router.get('/eliminar/:id', async(req, res, next) => {
+  var id = req.params.id;
+  await novedadesModel.deleteNovedadesById(id);
+  res.redirect('/admin/novedades');
+});
+
+/*la vista del form de modificar */
+
+router.get('/modificar/:id', async(req, res, next) => {
+  var id = req.params.id;
+  //console.log(req.params.id);
+  var novedad = await novedadesModel.getNovedadById(id);
+
+  //console.log(req.params.id);
+  res.render('admin/modificar', {
+    layout: 'admin/layout',
+    novedad
+  })
+})
+
+router.post('/modificar', async(req, res, next) => {
+  try {
+    var obj = {
+      titulo: req.body.titulo,
+      subtitulo: req.body.subtitulo,
+      cuerpo: req.body.cuerpo
+    }
+
+    await novedadesModel.modificarNovedadById(obj, req.body.id);
+    res.redirect('/admin/novedades');
+  } catch(error) {
+    res.render('admin/modificar', {
+      layout: 'admin/layout',
+      error: true,
+      message: 'No se modifico la novedad'
+    })
+  }
+})
 
 module.exports = router;
